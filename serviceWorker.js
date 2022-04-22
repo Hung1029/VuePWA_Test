a9b41aa4a310f2078ec3a1980f7a2745407f7a24
Hung1029 <psi94133@gmail.com>
2022-04-22 10:05:03 +0800

    dist

---
+++ serviceWorker.js
@@ -0,0 +1,51 @@
+// 當service worker在「安裝階段」時會觸發此事件
+self.addEventListener('install', function (event) {
+    self.skipWaiting();
+
+    var offlinePage = new Request('index.html');
+    var assest = ['/', './pwa-512x512.png', './pwa-192x192.png', './index.html', './assets/index.00cecb89.js', './favicon.ico', './manifest.json', './assets/index.6174dcc6.css', './assets/vendor.aaca27d0.js']
+    event.waitUntil(
+        fetch(assest).then(function (response) {
+            return caches.open('static').then(function (cache) {
+                cache.addAll(assest)
+                // return cache.put(assest, response);
+            });
+        }));
+});
+// 當頁面載入其他檔案資源時，觸發此事件
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function (response) {
+                //抓不到會拿到 null
+                if (response) {
+                    return response;
+                } else {
+                    return fetch(event.request);
+                }
+            })
+    );
+});
+
+self.addEventListener('refreshOffline', function (response) {
+    return caches.open('static').then(function (cache) {
+        return cache.put(offlinePage, response);
+    });
+});
+
+// self.addEventListener('push', function (event) {
+//     var data = event.data.json(); var opts = {
+//         body: data.body,
+//         icon: data.icon,
+//         data: {
+//             url: data.url
+//         }
+//     };
+//     event.waitUntil(self.registration.showNotification(data.title, opts));
+// });
+
+// self.addEventListener('notificationclick', function (event) {
+//     var data = event.notification.data; event.notification.close(); event.waitUntil(
+//         clients.openWindow(data.url)
+//     );
+// });
